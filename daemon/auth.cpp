GrapheneOS/platform_packages_modules_adb

Fix EVP_PKEY_cmp usage.
Not all non-zero values mean success.

Flag: EXEMPT CVE_FIX
Bug: 469080888
Test: b/469080888#comment6
Change-Id: I20f3abf93ab9bd0bd349d17cce2ba7f56fa88e36

daemon/auth.cpp
@@ -358,11 +358,13 @@ int adbd_tls_verify_cert(X509_STORE_CTX* ctx, std::string* auth_key) {
         bool verified = false;
         bssl::UniquePtr<EVP_PKEY> known_evp(EVP_PKEY_new());
         EVP_PKEY_set1_RSA(known_evp.get(), key);
-        if (EVP_PKEY_cmp(known_evp.get(), evp_pkey.get())) {
+        int cmp_result = EVP_PKEY_cmp(known_evp.get(), evp_pkey.get());
+        if (cmp_result == 1) {
             VLOG(AUTH) << "Matched auth_key=" << public_key;
             verified = true;
         } else {
-            VLOG(AUTH) << "auth_key doesn't match [" << public_key << "]";
+            VLOG(AUTH) << "auth_key doesn't match [" << public_key << "] EVP_PKEY_cmp returned "
+                       << cmp_result;
         }
         RSA_free(key);
         if (verified) {

CodeLinaro-mirror/la_platform_packages_modules_adb

(cherry picked from commit 8248d2b1f479aebc66ed7d96ba1f19a4144bc537)
Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:7700455c4f3be46eb24a9b01446bcd90e68fc59e
Merged-In: I20f3abf93ab9bd0bd349d17cce2ba7f56fa88e36
(cherry picked from commit 4492fa1affa1ceb00c198ac68c4d5d194c1e480d)

daemon/auth.cpp
@@ -336,11 +336,13 @@ int adbd_tls_verify_cert(X509_STORE_CTX* ctx, std::string* auth_key) {
         bool verified = false;
         bssl::UniquePtr<EVP_PKEY> known_evp(EVP_PKEY_new());
         EVP_PKEY_set1_RSA(known_evp.get(), key);
-        if (EVP_PKEY_cmp(known_evp.get(), evp_pkey.get())) {
+        int cmp_result = EVP_PKEY_cmp(known_evp.get(), evp_pkey.get());
+        if (cmp_result == 1) {
             LOG(INFO) << "Matched auth_key=" << public_key;
             verified = true;
         } else {
-            LOG(INFO) << "auth_key doesn't match [" << public_key << "]";
+            LOG(INFO) << "auth_key doesn't match [" << public_key << "] EVP_PKEY_cmp returned "
+                      << cmp_result;
         }
         RSA_free(key);
         if (verified) {